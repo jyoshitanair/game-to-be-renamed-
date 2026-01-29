f878a14c0fdf68644b9888ea4387468f8aced6b5
Jyoshita Nair <jyoshitanair123@gmail.com>
2026-01-28 19:44:36 -0800

    Co-authored-by: anyaluo-ui <anyaluo-ui@users.noreply.github.com>

---
+++ MyFirstGame/MyWPFGame/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWPFGame;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+            Image player = new Image
+            {
+                Width = 100,
+                Height = 100,
+                Source = new BitmapImage(new Uri("pack://application:,,,/Image/player.png", UriKind.Absolute))
+            };
+        GameCanvas.Children.Add(player);
+        Canvas.SetLeft(player, 100);
+        Canvas.SetTop(player, 100);
+
+    }
+}
